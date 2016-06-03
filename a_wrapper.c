9250864aa57a36f39ad1695bc66deb289bf45c69
Bradley Sadowsky <bradley.sadowsky@gmail.com>
2016-06-02 20:46:53 -0400

    a_wrapper.c fix

--- a_wrapper.c
+++ a_wrapper.c
@@ -3,18 +3,22 @@
 #include "adpadfs.h"
 
 int a_wrapper(void) {
+  char *len;
   int length;
   printf("Automated Adpadfs System Version 1.0.0\n");
   printf("Input Curse Length: ");
-  scanf("%d", &length);
+  scanf("%s", &len);
+    
+  long leng = strtol(len, NULL, 10);
+  length = leng;
   
   if ( isdigit(length) ) {
     curse(length);
     printf("\n");
   } else if ( isalpha(length) ) {
-    printf("Error: Input not recognized--Exiting.\n");
-  } else {
     printf("Error: Input not digit--Exiting.\n");
+  } else {
+    printf("Error: Input not recognized--Exiting.\n");
   }
   return 0;
 }
