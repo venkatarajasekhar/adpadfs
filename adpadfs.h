3209fc022845dfaa2d39ab63e449612eaa2d31cf
Bradley Sadowsky <bradley.sadowsky@gmail.com>
2016-06-10 21:09:12 -0400

    Version 3.2.0 Update

--- adpadfs.h
+++ adpadfs.h
@@ -8,8 +8,9 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
+#include <unistd.h>
 
-#define VER "3.1.0"
+#define VER "3.2.0"
 
 enum lstat {dead = 0, live = 1}; /*Life Status*/
 typedef enum lstat lstat;
@@ -18,6 +19,7 @@ int curse(int len);
 int mid_finger(void);
 int a_wrapper(void);
 int rd_file(char *argv[]);
+int chk_file(char *fname);
 void chk_egg(int egg);
 
 #endif
--- main.c
+++ main.c
@@ -2,6 +2,16 @@
   Written By: Bradley Sadowsky */
 #include "adpadfs.h"
 
+int chk_file(char *fname) {
+  int fexists; /*File exists; 0 or 1*/
+  if ( access(fname, F_OK) != -1 ) /*Check if file exists*/ {
+    fexists = 1; /*File exists*/
+  } else {
+    fexists = 0; /*File does not exist*/
+  }
+  return fexists;
+}
+
 int main(int argc, char *argv[]) {
   long dig; /*Stores output of strtol*/
   int len;  /*Stores length of curse, equal to dig*/
@@ -17,19 +27,22 @@ int main(int argc, char *argv[]) {
     }
     
   } else if ( argc == 3 ) {
+    int fexists = chk_file(argv[2]);
     int flen = strlen(argv[2]); /*Size of "filename" argument*/
     int same = strcmp(argv[1], "-f");
-    if ( same == 0 && isalpha(*argv[2]) && flen > 6 ) {
-      int end = strcmp(&argv[2][flen-6], ".curse"); /*Checks if ending is .curse*/
-      if ( end == 0 ) {
-        len = rd_file(argv);
-        curse(len);
-        printf("\n");
-      } else {
-        printf("Error: Not a curse file... Exiting.\n");
+    if ( same == 0 && isalpha(*argv[2]) && fexists == 1 ) {
+      if ( flen >= 6 ) {
+        int end = strcmp(&argv[2][flen-6], ".curse"); /*Checks if ending is .curse*/
+        if ( end == 0 ) {
+          len = rd_file(argv);
+          curse(len);
+          printf("\n");
+        } else {
+          printf("Error: Not a curse file... Exiting.\n");
+        }
       }
     } else {
-      printf("usage: %s [options] length\n", argv[0]);
+      printf("Error: File does not exist... Exiting.\n");
     }
     
   } else {
